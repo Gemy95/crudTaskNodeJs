7adb7db6f1bb4dbaa16c9369eb9c181715789cfc
aligamal <aligamal@3ddx.com>
2020-10-23 20:30:28 +0200

    NF: add eslint.json rules

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -31,21 +30,21 @@ app.use("/api/devices", deviceRouter);
 expressSwagger(swaggerConfig.options);
 
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+ let err = new Error("Not Found");
+ err.status = 404;
+ next(err);
 });
 
 // error handler
 // eslint-disable-next-line
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+ // render the error page
+ res.status(err.status || 500);
+ res.render("error");
 });
 
 
